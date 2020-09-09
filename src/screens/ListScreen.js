614534a304d0470a26151afca302de21a8f738a6
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2020-09-09 14:24:34 +0800

    rendering list

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,8 +1,35 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 
 const ListScreen = () => {
-  return <Text>List Screen</Text>;
+  const friedns = [
+    { name: "Friend #1" },
+    { name: "Friend #2" },
+    { name: "Friend #3" },
+    { name: "Friend #4" },
+    { name: "Friend #5" },
+    { name: "Friend #6" },
+    { name: "Friend #7" },
+    { name: "Friend #8" },
+    { name: "Friend #9" },
+    { name: "Friend #10" },
+    { name: "Friend #11" },
+    { name: "Friend #12" },
+    { name: "Friend #13" },
+  ];
+
+  return (
+    <FlatList
+      data={friedns}
+      renderItem={({ item }) => {
+        return (
+          <View>
+            <Text>{item.name}</Text>
+          </View>
+        );
+      }}
+    />
+  );
 };
 
 const styels = StyleSheet.create({});
